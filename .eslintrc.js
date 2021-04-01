98e6f5a1e4cfac9d9effede71de5ddf04b48615b
Nolan Kataoka <nmkataoka@gmail.com>
2021-04-01 17:23:28 -0400

    Remove required type checking eslint package

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:import/typescript',
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
